79847d2cc2bed15c0ee7dc666796d7c5dfd6c3aa
Krupa Patel <53213815+krupa253@users.noreply.github.com>
2022-03-30 19:39:30 +0530

    start Component Hand Book

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material_handbook';
+  name = 'Angular';
+
+  onButtonClicked(event: any) {
+   console.log(event); // handle button clicked here.
+  }
+
 }
